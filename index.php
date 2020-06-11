7863460ff2f3cc73dea6eaa34b3e1b3695f9a45f
Rock Lee <1053166921@qq.com>
2020-06-11 21:58:22 +0800

    init

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
-<?php 
-	echo "message";
-	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>留言板</title>
+</head>
+<body>
+
+<h1>留言板</h1>
+<form action="./message.php" method="post">
+	<table>
+		<tr>
+		<td>
+			联系人:
+		</td>
+		<td>
+			<input type="text" name="username" />	
+		</td>
+	</tr>
+	<tr>
+		<td>
+			联系方式:
+		</td>	
+		<td>
+			<input type="text" name="phone" />	
+		</td>
+	</tr>
+	<tr>
+		<td>
+			留言:
+		</td>
+		<td>
+			<textarea rows="4" name="message"></textarea>	
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2"><input type="submit" name="" value="提交"></td>
+	</tr>
+	</table>
+	
+</form>
+</body>
+</html>
